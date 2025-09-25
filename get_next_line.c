374d8538822791d84e7441697fd6301d305ecfc9
Mohammed Zaher <mohammedzaher9087@gmail.com>
2025-09-25 14:32:27 +0300

    almostFinished

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,108 @@
 /*   By: moatieh <moatieh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 23:20:02 by moatieh           #+#    #+#             */
-/*   Updated: 2025/09/19 23:20:02 by moatieh          ###   ########.fr       */
+/*   Updated: 2025/09/25 14:29:38 by moatieh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "get_next_line.h"
 
-#define BUFFER 10
-
-char    *get_next_line(int fd)
+static char	*line_maker(char *rem)
 {
-	char *b = calloc(BUFFER, 1);
-	char *rem;
-	int count;
+	char	*line;
+	int		i;
 
-	while(count != 0)
+	i = 0;
+	while (rem[i] && rem[i] != '\n')
+		i++;
+	if (rem[i] == '\n')
+		i++;
+	line = ft_substr(rem, 0, i);
+	if (!line)
+		return (NULL);
+	return (line);
+}
+
+static char	*rem_saver(char *rem)
+{
+	char	*new_rem;
+	int		i;
+
+	i = 0;
+	while (rem[i] && rem[i] != '\n')
+		i++;
+	if (rem[i] == '\n')
+		i++;
+	if (rem[i] == '\0')
 	{
-		count = read(fd, b, BUFFER);
-		printf("%s", b);
+		free(rem);
+		return (NULL);
 	}
-	return NULL;
+	new_rem = ft_substr(rem, i, ft_strlen(rem) - i);
+	if (!new_rem)
+		return (NULL);
+	free(rem);
+	return (new_rem);
 }
 
-int main()
+static char	*reader(int fd, char *rem)
 {
-	int fd = open("text.txt", O_RDONLY);
-	get_next_line(fd);
+	char	*buffer;
+	int		bytes;
+
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes = 1;
+	while ((!ft_strchr(rem, '\n') && bytes) > 0 || !rem)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+		{
+			free(buffer);
+			free(rem);
+			return (NULL);
+		}
+		buffer[bytes] = '\0';
+		rem = ft_strjoin(rem, buffer);
+	}
+	free(buffer);
+	return (rem);
 }
+
+char	*get_next_line(int fd)
+{
+	char		*one_line;
+	char		*buffer;
+	static char	*rem;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	rem = reader(fd, rem);
+	free(buffer);
+	if (!rem || rem[0] == '\0')
+		return (NULL);
+	one_line = line_maker(rem);
+	rem = rem_saver(rem);
+	return (one_line);
+}
+
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+
+// 	fd = open("text.txt", O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		perror("Cannot open file");
+// 		return (1);
+// 	}
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	close(fd);
+// 	return (0);
+// }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,28 +6,30 @@
 /*   By: moatieh <moatieh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 23:19:58 by moatieh           #+#    #+#             */
-/*   Updated: 2025/09/19 23:19:58 by moatieh          ###   ########.fr       */
+/*   Updated: 2025/09/25 14:24:39 by moatieh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+// void* ft_malloc(size_t size)
+// {
+//     static size_t call = 0;
 
-void* ft_malloc(size_t size)
-{
-    static size_t call = 0;
-
-    call++;
-    if (call % 2 == 1)
-        return NULL;
-    else
-        return malloc(size);        
-}
+//     call++;
+//     if (call % 2 == 1)
+//         return (NULL);
+//     else
+//         return (malloc(size));
+// }
 
 int	ft_strlen(const char *s)
 {
 	int	count;
 
 	count = 0;
+	if (!s)
+		return (0);
 	while (s[count] != '\0')
 	{
 		count++;
@@ -38,11 +40,11 @@ int	ft_strlen(const char *s)
 char	*ft_strdup(const char *s)
 {
 	char	*cpy;
-	int	slen;
+	size_t	slen;
 	int		count;
 
 	slen = ft_strlen(s);
-	cpy = ft_malloc((slen + 1) * sizeof(char));
+	cpy = (char *)malloc((slen + 1) * sizeof(char));
 	if (!cpy)
 		return (NULL);
 	count = 0;
@@ -53,4 +55,98 @@ char	*ft_strdup(const char *s)
 	}
 	cpy[count] = '\0';
 	return (cpy);
+}
+
+static char	*cpy(char *str, char const *first, char const *sec)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (!first)
+		first = "";
+	while (first[i])
+	{
+		str[i] = first[i];
+		i++;
+	}
+	j = 0;
+	if (!sec)
+		sec = "";
+	while (sec[j])
+	{
+		str[i + j] = sec[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	total;
+
+	i = 0;
+	total = 0;
+	while (s1 && s1[total])
+		total++;
+	while (s2 && s2[i])
+		i++;
+	total += i;
+	str = (char *)malloc((total + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	str = cpy(str, s1, s2);
+	return (str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	slen;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start >= slen)
+	{
+		str = (char *)malloc(1);
+		str[0] = '\0';
+		return (str);
+	}
+	if (len > slen - start)
+		len = slen - start;
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (i < len && s[start])
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	str[i] = '\0';
+	return str;
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	count;
+
+	count = 0;
+	if (!s)
+		return (NULL);
+	while (s[count] != '\0')
+	{
+		if ((unsigned char)s[count] == ((unsigned char)c))
+			return ((char *)(s + count));
+		count++;
+	}
+	if (c == '\0')
+		return ((char *)(s + count));
+	return (NULL);
 }
